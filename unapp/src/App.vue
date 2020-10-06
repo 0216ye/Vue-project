99c3093757a8f26ca4b45eecae150cd55b21eaeb
yongjie <2246106715@qq.com>
2020-10-06 09:14:54 +0800

    初始化项目准备

---
+++ unapp/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>
+    组件
+  </h1>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+  }
+</script>
+<style lang="stylus" scoped>
+  h1 
+   color red
+</style>  
